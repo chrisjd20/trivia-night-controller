a9e9fde57ee1367237dd8cf648226d9ad949de0f
chrisjd20 <chrisjd20@gmail.com>
2025-05-05 15:54:31 -0400

    Delete static/test.js

--- static/test.js
+++
@@ -1,6 +0,0 @@
-fetch("/radrovers/transfer", {
-  method: "POST",
-  credentials: "include",
-  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  body: "from_account=VT-7731-2277-0451&to_account=VT-1234-1234-1234&amount=100000"
-})
